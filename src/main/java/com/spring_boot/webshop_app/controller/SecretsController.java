d08f53959efeb8b6285ca226392b40bbfd914c0d
Filip Miloš <107814991+mikiqqqq@users.noreply.github.com>
2024-06-27 10:40:26 +0200

    updates

--- src/main/java/com/spring_boot/webshop_app/controller/SecretsController.java
+++ src/main/java/com/spring_boot/webshop_app/controller/SecretsController.java
@@ -2,7 +2,6 @@ package com.spring_boot.webshop_app.controller;
 
 import com.azure.security.keyvault.secrets.SecretClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,18 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/secrets")
 public class SecretsController {
 
-    private final SecretClient secretClient;
     private final String sasTokenName;
 
     @Autowired
-    public SecretsController(SecretClient secretClient, @Value("${azure.keyvault.sas-token-name}") String sasTokenName) {
-        this.secretClient = secretClient;
-        this.sasTokenName = sasTokenName;
+    public SecretsController(SecretClient secretClient) {
+        this.sasTokenName = secretClient.getSecret("sas-token").getValue();
     }
 
     @GetMapping("/sas-token")
     public ResponseEntity<String> getSasToken() {
-        String sasToken = secretClient.getSecret(sasTokenName).getValue();
-        return ResponseEntity.ok(sasToken);
+        return ResponseEntity.ok(sasTokenName);
     }
 }
