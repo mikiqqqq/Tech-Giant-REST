30d4bdcecd6bac35708050e9069c6157376edfb3
Filip Miloš <107814991+mikiqqqq@users.noreply.github.com>
2024-07-11 22:06:25 +0200

    updates

--- src/main/java/com/spring_boot/webshop_app/config/KeyVaultConfig.java
+++ src/main/java/com/spring_boot/webshop_app/config/KeyVaultConfig.java
@@ -3,16 +3,20 @@ package com.spring_boot.webshop_app.config;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KeyVaultConfig {
 
+    @Value("${spring.cloud.azure.keyvault.secret.endpoint}")
+    private String keyVaultUrl;
+
     @Bean
     public SecretClient secretClient() {
         return new SecretClientBuilder()
-                .vaultUrl("https://techgiant.vault.azure.net/")
+                .vaultUrl(keyVaultUrl)
                 .credential(new DefaultAzureCredentialBuilder().build())
                 .buildClient();
     }
